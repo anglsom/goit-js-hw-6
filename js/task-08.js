06d267a0963d038732d59249686faa064500cc2d
anglsom <anastasiyaladkovskaya@gmail.com>
2022-08-18 21:25:04 +0200

    update

--- js/task-08.js
+++ js/task-08.js
@@ -26,25 +26,6 @@ function onSubmit(event) {
     
 
 
-// const formEl = document.querySelector('.login-form');
-
-// formEl.addEventListener('submit', onHeandlSubmit);
-
-// function onHeandlSubmit(event) {
-//     event.preventDefault();
-//     const { email, password } = event.currentTarget.elements;
-//     if (email.value.trim() === '' || password.value.trim() === '')
-//     return alert('Fill in the fields')
-
-//     const formData = new FormData(event.currentTarget);
-//     const dataFormValue = {};
-
-//     formData.forEach((value, name) => {
-//     dataFormValue[name] = value;
-// })
-// console.log(dataFormValue);
-//     formEl.reset();
-// };
 
 
 
